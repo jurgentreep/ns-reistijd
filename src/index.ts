decafc31a58a84c47c24cbebaf9395e6764075af
Jurgen Treep <jurgen.treep@gmail.com>
2019-06-18 00:10:02 +0200

    parse dates

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 const form = document.querySelector('form');
 const input = document.querySelector('input');
 import { parse } from 'papaparse';
+import moment from 'moment';
+
+// This format doesn't make much sense if you think about it
+const dateFormat = 'HH:mm DD-MM-YYYY';
 
 if (form) {
   form.addEventListener('submit', event => {
@@ -9,7 +13,12 @@ if (form) {
     if (input && input.files && input.files.length) {
       parse(input.files[0], {
         complete: ({ data }) => {
-          console.table(data);
+          const departureString = `${data[1][1]} ${data[1][0]}`
+          const arrivalString = `${data[1][3]} ${data[1][0]}`
+          const departure = moment(departureString, dateFormat);
+          const arrival = moment(arrivalString, dateFormat);
+          console.log(departure);
+          console.log(departure.toISOString())
         }
       })
     }
