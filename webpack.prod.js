7029d816711497ef3403b4c7e42468475b010453
Jurgen Treep <jurgen.treep@gmail.com>
2019-06-19 16:50:55 +0200

    add versioning for better caching

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,25 @@
+const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
+  plugins: [
+    new webpack.HashedModuleIdsPlugin()
+  ],
   mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js'
+  },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    }
+  }
 });
